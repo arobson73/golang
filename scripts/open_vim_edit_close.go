bc1a4a9309e5224d5391437c32ce85b5e9d621e4
andyr <arobson73@users.noreply.github.com>
2017-08-18 07:55:56 +0100

    Create open_vim_edit_close.go

---
+++ scripts/open_vim_edit_close.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	editor := os.Getenv("EDITOR")
+	if editor == "" {
+		editor = "vim"
+	}
+	editorPath, err := exec.LookPath(editor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tempfile, err := ioutil.TempFile("", "issue_crud")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tempfile.Close()
+	defer os.Remove(tempfile.Name())
+
+	cmd := &exec.Cmd{
+		Path:   editorPath,
+		Args:   []string{editor, tempfile.Name()},
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	}
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
