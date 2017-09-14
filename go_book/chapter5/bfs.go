971aebc25a7c8b5c85a7ef881c401d256cbd31d4
andyr <forpetessake@protonmail.com>
2017-09-14 21:21:09 +0100

    Create bfs.go

---
+++ go_book/chapter5/bfs.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+
+	breadthFirst := func(f func(item string) []string, worklist []string) {
+		seen := make(map[string]bool)
+		for len(worklist) > 0 {
+			items := worklist
+			worklist = nil
+			for _, item := range items {
+				if !seen[item] {
+					seen[item] = true
+					worklist = append(worklist, f(item)...)
+				}
+			}
+		}
+	}
+
+	pre := func(course string) []string {
+		fmt.Println(course)
+		return prereqs[course]
+	}
+
+	var course string
+	for course = range prereqs { // get random key
+
+		fmt.Print("\nPrereqs for:")
+		breadthFirst(pre, []string{course})
+		fmt.Println("")
+	}
+
+}
