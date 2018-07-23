779e7b33f4ac2c9dd65e076c2c1f597f096dca29
Poodle <forpetessake@protonmail.com>
2018-07-23 14:11:08 +0100

    Add files via upload

---
+++ rest_with_mongo/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/PacktPublishing/Cloud-Native-programming-with-Golang/Chapter02/myevents/src/eventsservice/rest"
+	"github.com/PacktPublishing/Cloud-Native-programming-with-Golang/Chapter02/myevents/src/lib/configuration"
+	"github.com/PacktPublishing/Cloud-Native-programming-with-Golang/Chapter02/myevents/src/lib/persistence/dblayer"
+)
+
+func main() {
+
+	confPath := flag.String("conf", `.\configuration\config.json`, "flag to set the path to the configuration json file")
+	flag.Parse()
+	//extract configuration
+	config, _ := configuration.ExtractConfiguration(*confPath)
+
+	fmt.Println("Connecting to database")
+	dbhandler, _ := dblayer.NewPersistenceLayer(config.Databasetype, config.DBConnection)
+	//RESTful API start
+	fmt.Println("Here")
+	log.Fatal(rest.ServeAPI(config.RestfulEndpoint, dbhandler))
+}
