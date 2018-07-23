7fc06dc8467e161557e15a5d539cf976b97e6f5b
Poodle <forpetessake@protonmail.com>
2018-07-23 14:23:50 +0100

    Create persistence.go

---
+++ rest_with_mongo/lib/persistence/persistence.go
@@ -0,0 +1,9 @@
+package persistence
+
+type DatabaseHandler interface {
+	AddEvent(Event) ([]byte, error)
+	FindEvent([]byte) (Event, error)
+	FindEventByName(string) (Event, error)
+	FindAllAvailableEvents() ([]Event, error)
+	DeleteEventByName(string) error
+}
