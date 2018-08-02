e09cf9c3676de094cf84149cf4034b762843486b
Poodle <forpetessake@protonmail.com>
2018-08-02 21:38:43 +0100

    Create rest.go

---
+++ booking/eventService/rest/rest.go
@@ -0,0 +1,33 @@
+package rest
+
+import (
+	"net/http"
+
+	"andy/booking/lib/msgqueue"
+	"andy/booking/lib/persistence"
+
+	"github.com/gorilla/mux"
+)
+
+func ServeAPI(endpoint string, dbHandler persistence.DatabaseHandler, eventEmitter msgqueue.EventEmitter) error {
+	handler := newEventHandler(dbHandler, eventEmitter)
+
+	r := mux.NewRouter()
+	eventsrouter := r.PathPrefix("/events").Subrouter()
+	eventsrouter.Methods("GET").Path("/{SearchCriteria}/{search}").HandlerFunc(handler.findEventHandler)
+	eventsrouter.Methods("GET").Path("").HandlerFunc(handler.allEventHandler)
+	eventsrouter.Methods("GET").Path("/{eventID}").HandlerFunc(handler.oneEventHandler)
+	eventsrouter.Methods("POST").Path("").HandlerFunc(handler.newEventHandler)
+
+	locationRouter := r.PathPrefix("/locations").Subrouter()
+	locationRouter.Methods("GET").Path("").HandlerFunc(handler.allLocationsHandler)
+	locationRouter.Methods("POST").Path("").HandlerFunc(handler.newLocationHandler)
+
+	//users not sure about this yet (as in how it fits into grand scheme of things and whether the route is correct etc
+	userRouter := r.PathPrefix("/users").Subrouter() //this uses AddUser for db
+
+	userRouter.Methods("POST").Path("").HandlerFunc(handler.newUserHandler)
+	userRouter.Methods("GET").Path("/findUser/{firstname}/{secondname}").HandlerFunc(handler.findUserHandler)
+
+	return http.ListenAndServe(endpoint, r)
+}
