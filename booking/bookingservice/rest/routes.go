6b7e2d85e65b448480c9060e8e14e5d3e78992f8
Poodle <forpetessake@protonmail.com>
2018-08-19 16:19:46 +0100

    Add files via upload

--- booking/bookingservice/rest/routes.go
+++ booking/bookingservice/rest/routes.go
@@ -8,15 +8,17 @@ import (
 	"andy/booking/lib/msgqueue"
 	"andy/booking/lib/persistence"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
 func ServeAPI(listenAddr string, database persistence.DatabaseHandler, eventEmitter msgqueue.EventEmitter) {
 	r := mux.NewRouter()
 	r.Methods("post").Path("/events/{eventID}/{userID}/bookings").Handler(&CreateBookingHandler{eventEmitter, database})
+	rc := handlers.CORS()(r)
 
 	srv := http.Server{
-		Handler:      r,
+		Handler:      rc,
 		Addr:         listenAddr,
 		WriteTimeout: 2 * time.Second,
 		ReadTimeout:  1 * time.Second,
