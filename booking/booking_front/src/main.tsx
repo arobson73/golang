64f7651512861ad3249c8ca1905c376868ff5c01
Poodle <forpetessake@protonmail.com>
2018-09-03 20:01:12 +0100

    Add files via upload

--- booking/booking_front/src/main.tsx
+++ booking/booking_front/src/main.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Switch, Route,BrowserRouter,HashRouter} from 'react-router-dom';
+import { Router,Switch, Route,BrowserRouter,HashRouter } from 'react-router-dom';
 import { Login } from './components/Login';
 import { Register } from './components/Register';
 import { EventBookingFormContainer } from './components/event_booking_form_container';
 import { EventListContainer } from "./components/event_list_container";
 import { BookingListContainer } from "./components/booking_list_container";
 import {ErrorLogin} from './components/error'
+import {AdminLogin} from './components/admin_login'
+import {AdminEvent} from './components/admin_events'
 
 class App extends React.Component<{}, {}> {
   render() {
@@ -14,17 +16,21 @@ class App extends React.Component<{}, {}> {
     const userBookings = ({match}:any,props) => <BookingListContainer bookingServiceURL="http://localhost:8182" userid={match.params.userid} {...props}/>
     const eventBooking = ({ match }: any) => <EventBookingFormContainer userID={match.params.userid} eventID={match.params.id} eventServiceURL="http://localhost:8181"
       bookingServiceURL="http://localhost:8182" />;
+    const adminEvent = (props) => <AdminEvent {...props}/>  
 
-    return <HashRouter>
-      <Switch>
+    return <HashRouter >
+    <Switch>
         <Route exact={true} path="/" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/list" component={eventList} />
         <Route path="/error" component={ErrorLogin}/>
         <Route path="/bookings/:id/:userid" component={eventBooking}/>
         <Route path="/userbookings/:userid" component={userBookings} />
+        <Route exact={true} path="/admin" component={AdminLogin} />
+        <Route path="/admin/event" component={adminEvent}/>
+  {/* <Route path="/admin/eventsCreated" component={AdminEventsCreated}/>*/}
 
-      </Switch>
+        </Switch>
     </HashRouter>
   }
 }
